438dbb7b272be7aa11f3be8593807f01852a3a48
Nicklas <nicklascschmidt@Nicklass-MacBook-Pro.local>
2019-04-15 20:24:30 -0700

    server cleaned up

--- server.js
+++ server.js
@@ -5,36 +5,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-// Bring in routes, initialize express, define dev port
-// const routes = require("./routes/api-routes.js");
-
-
+// Initialize express, define dev port, bring in API routes
 const app = express();
 const port = process.env.PORT || 5000;
-
 require("./routes/api-routes.js")(app);
-
-// ------------------------------------------------------
-// ------------------- Server --------------------------
-// ------------------------------------------------------
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// const routes = require('./routes');
-// app.use('/', routes);
-
-// Static directory
+// Serve static directory
 app.use(express.static('client'));
-// app.use(routes);
 
-
+// Serve static files, handle React routing, return all requests to the React app
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
   app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
-app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
